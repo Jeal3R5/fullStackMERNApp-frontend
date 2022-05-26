5ed0fa56210672ec6c54c98c846d86ee6d8572d9
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-25 20:37:07 -0700

    frontend commit index

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 function Main(props) {
+  const [people, setPeople] = useState(null);
+
+  const URL = "http://localhost:3001/people/";
+  const getPeople = async () => {
+    const data = await fetch(URL).then((res) => res.json());
+    setPeople(data);
+  };
+  const createPeople = async (person) => {
+    //make post request to create people
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    getPeople();
+  };
+  useEffect(() => {
+    getPeople();
+  }, []);
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/people/:id" element={<Show />} />
+        <Route
+          path="/"
+          element={<Index people={people} createPeople={createPeople} />}
+        />
+        <Route path="/people/:id" element={<Show people={people} />} />
       </Routes>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Link } from "react-router-dom";
+
 // IMPORT SCSS FILE TO BE SOURCE OF STYLING
 import "./styles.scss";
 // IMPORT ROUTER
@@ -20,3 +22,22 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function Index({ people, createPeople }) {
+  //loaded function
+  const loaded = () => {
+    return people.map((person) => (
+      <div key={person._id} className="person">
+        <Link to={`/people/${person._id}`}>
+          <h1>{person.name}</h1>
+        </Link>
+        <img src={person.image} alt="" />
+        <h3>{person.title}</h3>
+      </div>
+    ));
+  };
+  const loading = () => <h1>Loading...</h1>;
+  return people ? loaded() : loading();
+}
+
+export default Index;
